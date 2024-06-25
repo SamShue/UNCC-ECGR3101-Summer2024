824d8617b3763faa33f65140b319186cc80d9637
Sam Shue <evilrobotfactory@gmail.com>
2024-06-25 18:17:34 -0400

    Update msp430g2xx3_1.c

--- FSM_Virtual_Train_Example/msp430g2xx3_1.c
+++ FSM_Virtual_Train_Example/msp430g2xx3_1.c
@@ -1,11 +1,61 @@
 #include <msp430.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include "ws2812.h"
 
-void gradualFill(u_int n, u_char r, u_char g, u_char b);
+void init();
 void setTrainPosition(uint16_t pos);
 
+typedef enum {STOP, FORWARD, REVERSE, NUM_STATES} state_t;
+typedef enum {FORWARDS, BACKWARDS} direction_t;
+
+typedef struct {
+    direction_t direction;
+    bool isLongPress;
+    bool isShortPress;
+    uint16_t position;
+} inputs_t;
+
+state_t runStopState(inputs_t*);
+state_t runForwardState(inputs_t*);
+state_t runReverseState(inputs_t*);
+
+inputs_t inputs;
+
 int main(void) {
+    init();
+
+    setTrainPosition(inputs.position);
+
+    state_t currentState = STOP;
+    state_t nextState = currentState;
+
+    inputs.direction = FORWARDS;
+    inputs.isLongPress = false;
+    inputs.isShortPress = false;
+    inputs.position = 0;
+
+    volatile uint16_t pos = 0;
+    while (1) {
+        switch(currentState){
+        case STOP:
+            nextState = runStopState(&inputs);
+            break;
+        case FORWARD:
+            nextState = runForwardState(&inputs);
+            break;
+        case REVERSE:
+            nextState = runReverseState(&inputs);
+            break;
+        default:
+            break;
+        }
+
+        currentState = nextState;
+    }
+}
+
+void init(){
     WDTCTL = WDTPW + WDTHOLD;  // Stop WDT
     if (CALBC1_16MHZ==0xFF)    // If calibration constant erased
     {
@@ -19,21 +69,69 @@ int main(void) {
     // initialize LED strip
     initStrip();
 
-    volatile uint16_t pos = 0;
-    while (1) {
-        setTrainPosition(pos);
-        pos = (pos + 1) % NUM_LEDS;
-        __delay_cycles(1000000);
-    }
+    // Button config
+    P1DIR &= ~BIT3;
+    P1REN |= BIT3;
+    P1OUT |= BIT3;
+
+    P1IES |= BIT3;  // high to low trigger
+    P1IE |= BIT3;   // enable interrupt
+    __enable_interrupt();   // global enable
 }
 
-void gradualFill(u_int n, u_char r, u_char g, u_char b){
-    int i;
-    for (i = 0; i < n; i++){        // n is number of LEDs
-        setLEDColor(i, r, g, b);
-        showStrip();
-        _delay_cycles(1000000);       // lazy delay
+state_t runStopState(inputs_t* inputs){
+    // Do nothing
+    if(inputs->isLongPress){
+        inputs->isLongPress = false;
+        inputs->direction = !inputs->direction; // Flip the direction
     }
+
+    if(inputs->isShortPress){
+        inputs->isShortPress = false;
+        if(inputs->direction == FORWARDS){
+            return FORWARD;
+        } else {
+            return REVERSE;
+        }
+    }
+    return STOP;
+}
+
+state_t runForwardState(inputs_t* inputs){
+    setTrainPosition(inputs->position);
+    inputs->position = (inputs->position + 1) % NUM_LEDS;
+    __delay_cycles(1000000);
+
+    if(inputs->isShortPress){
+        inputs->isShortPress = false;
+        return STOP;
+    }
+
+    if(inputs->isLongPress){
+        inputs->isLongPress = false;
+        inputs->direction = BACKWARDS;
+        return REVERSE;
+    }
+    return FORWARD;
+}
+
+state_t runReverseState(inputs_t* inputs){
+    setTrainPosition(inputs->position);
+    inputs->position = (inputs->position - 1) % NUM_LEDS;
+    __delay_cycles(1000000);
+
+    if(inputs->isShortPress){
+        inputs->isShortPress = false;
+        return STOP;
+    }
+
+    if(inputs->isLongPress){
+        inputs->isLongPress = false;
+        inputs->direction = FORWARDS;
+        return FORWARD;
+    }
+
+    return REVERSE;
 }
 
 void setTrainPosition(uint16_t pos){
@@ -44,4 +142,19 @@ void setTrainPosition(uint16_t pos){
     for(i = 0; i < TRAIN_LEN; i++)
         setLEDColor((pos + i) % NUM_LEDS, BRIGHTNESS, BRIGHTNESS, BRIGHTNESS);
     showStrip();
+}
+
+#pragma vector=PORT1_VECTOR
+__interrupt void Port1ISR(){
+
+    if(P1IFG & BIT3){
+        __delay_cycles(16000000);   // Shouldn't do this!
+
+        if(P1IN & BIT3){ // button is no longer pressed
+            inputs.isShortPress = true;
+        }
+
+        inputs.isLongPress = true;
+        P1IFG &= ~BIT3;
+    }
 }
