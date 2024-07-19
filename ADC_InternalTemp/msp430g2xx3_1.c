d998ca66dcee28985c3589d4e517a810c140482f
Sam Shue <evilrobotfactory@gmail.com>
2024-07-19 11:08:17 -0400

    updated ADC examples

--- ADC_InternalTemp/msp430g2xx3_1.c
+++ ADC_InternalTemp/msp430g2xx3_1.c
@@ -1,29 +1,22 @@
 #include <msp430.h>
-
-long temp;
-long IntDegF;
-long IntDegC;
+#include <stdint.h>
 
 int main(void) {
     WDTCTL = WDTPW + WDTHOLD;
 
     ADC10CTL1 = INCH_10 | ADC10DIV_3;         // Temp Sensor ADC10CLK/4
     ADC10CTL0 = SREF_1 | ADC10SHT_3 | REFON | ADC10ON;
-    __enable_interrupt();
 
     while(1){
         ADC10CTL0 |= ENC | ADC10SC;             // Sampling and conversion start
         while(ADC10CTL1 & ADC10BUSY);
 
-        // oF = ((A10/1024)*1500mV)-923mV)*1/1.97mV = A10*761/1024 - 468
-        temp = ADC10MEM;
-        IntDegF = ((temp - 630) * 761) / 1024;
+        volatile uint16_t adcVal = ADC10MEM;
 
-        // oC = ((A10/1024)*1500mV)-986mV)*1/3.55mV = A10*423/1024 - 278
-        temp = ADC10MEM;
-        IntDegC = ((temp - 673) * 423) / 1024;
+        volatile float voltage = ((float)adcVal) * (1.5/1024.0);
+
+        volatile float degC = (voltage - 0.986)/0.00355;
 
         __no_operation();                       // SET BREAKPOINT HERE
-
     }
 }
