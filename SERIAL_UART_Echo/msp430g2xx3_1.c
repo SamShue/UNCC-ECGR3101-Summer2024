74ccca05100a43c83e1f2a4b995213ff57b02d92
Sam Shue <evilrobotfactory@gmail.com>
2024-11-04 09:14:42 -0500

    Last Push

--- SERIAL_UART_Echo/msp430g2xx3_1.c
+++ SERIAL_UART_Echo/msp430g2xx3_1.c
@@ -1,20 +1,20 @@
 #include <msp430.h>
 
 int main(void) {
-    WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+    WDTCTL = WDTPW + WDTHOLD;               // Stop WDT
 
-    P1SEL = BIT1 | BIT2 ;                     // P1.1 = RXD, P1.2=TXD
+    P1SEL = BIT1 | BIT2;                    // P1.1 = RXD, P1.2=TXD
     P1SEL2 = BIT1 | BIT2;
-    UCA0CTL1 |= UCSSEL_2;                     // SMCLK
-    UCA0BR0 = 8;                              // 1MHz 115200
-    UCA0BR1 = 0;                              // 1MHz 115200
-    UCA0MCTL = UCBRS2 + UCBRS0;               // Modulation UCBRSx = 5
-    UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
-    IE2 |= UCA0RXIE;                           // Set P1.0 to output direction
+    UCA0CTL1 |= UCSSEL_2;                   // SMCLK
+    UCA0BR0 = 8;                            // 1MHz 115200
+    UCA0BR1 = 0;                            // 1MHz 115200
+    UCA0MCTL = UCBRS2 | UCBRS1;             // Modulation UCBRSx = 6
+    UCA0CTL1 &= ~UCSWRST;                   // **Initialize USCI state machine**
+    IE2 |= UCA0RXIE;                        // Enable USCI_A0 RX interrupt
 
     __enable_interrupt();
 
-    UCA0TXBUF = 'A';                    // TX -> RXed character
+    UCA0TXBUF = 'A';                        // Print 'A' to UART
     while(1){
 
     }
@@ -22,6 +22,6 @@ int main(void) {
 
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR(void){
-    while (!(IFG2&UCA0TXIFG));                // USCI_A0 TX buffer ready?
-    UCA0TXBUF = UCA0RXBUF;                    // TX -> RXed character
+    while (!(IFG2&UCA0TXIFG));              // USCI_A0 TX buffer ready?
+    UCA0TXBUF = UCA0RXBUF;                  // TX -> RXed character
 }
--- TIMERS_PWM/msp430g2xx3_1.c
+++ TIMERS_PWM/msp430g2xx3_1.c
@@ -6,12 +6,19 @@ int main(void){
   P1DIR |= BIT6;    // Red LED Output
   P1SEL |= BIT6;    // Red LED PWM
 
+  BCSCTL3 = LFXT1S_2;                      // LFXT1 = VLO
+  IFG1 &= ~OFIFG;                           // Clear OSCFault flag
+
   TA0CCR0 = 512;    // PWM Period
   TA0CCR1 = 100;    // PWM duty cycle
   TA0CCTL1 = OUTMOD_7;  // TA0CCR1 high voltage below count, low voltage above count
 
-  TA0CTL = TASSEL_2 | MC_1 | ID_3;
+  TA0CTL = TASSEL_1 | MC_1 | ID_3;
 
+
+  __bis_SR_register(LPM3_bits);     // Enter LPM3
+  /*
   while(1){
   }
+  */
 }
